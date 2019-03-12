93a85e1b49c4af7fe13bb454289a483908f0d4a9
David <Daveysdownfall@gmail.com>
2019-03-12 13:41:57 -0400

    Created editor component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EditorComponent } from './editor/editor.component';
+import { QuestionCreatorComponent } from './question-creator/question-creator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorComponent,
+    QuestionCreatorComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/question-creator/question-creator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-question-creator',
+  templateUrl: './question-creator.component.html',
+  styleUrls: ['./question-creator.component.scss']
+})
+export class QuestionCreatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
