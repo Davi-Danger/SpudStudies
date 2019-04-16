c71a4ab8c751f745dfbd423bb3b2b6af4cecde2d
David <Daveysdownfall@gmail.com>
2019-04-16 11:47:45 -0400

    Added question component +Minor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {PracticeComponent} from './practice/practice.component';
 import {QuestionCreatorComponent} from './question-creator/question-creator.component';
 import {TabsComponent} from './tabs/tabs.component';
+import { QuestionComponent } from './question/question.component';
 
 const appRoutes: Routes = [
   {path: 'edit', component: EditorComponent},
@@ -20,7 +21,7 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent, EditorComponent, QuestionCreatorComponent, PracticeComponent,
-    TabsComponent, PageNotFoundComponent
+    TabsComponent, PageNotFoundComponent, QuestionComponent
   ],
   imports: [
     BrowserModule, FormsModule,
