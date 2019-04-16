b0f9e6a2838c8aa3a31419c5003852394bc139ce
David <Daveysdownfall@gmail.com>
2019-04-16 11:41:09 -0400

    Added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,33 @@
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule, Routes} from '@angular/router';
 
 import {AppComponent} from './app.component';
 import {EditorComponent} from './editor/editor.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {PracticeComponent} from './practice/practice.component';
 import {QuestionCreatorComponent} from './question-creator/question-creator.component';
+import {TabsComponent} from './tabs/tabs.component';
+
+const appRoutes: Routes = [
+  {path: 'edit', component: EditorComponent},
+  {path: 'practice', component: PracticeComponent},
+  {path: '', redirectTo: '/practice', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent, EditorComponent, QuestionCreatorComponent, PracticeComponent
+    AppComponent, EditorComponent, QuestionCreatorComponent, PracticeComponent,
+    TabsComponent, PageNotFoundComponent
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule, FormsModule,
+    RouterModule.forRoot(
+        appRoutes, {enableTracing: true}  // <-- debugging purposes only
+        )
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
