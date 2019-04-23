12cb8332c0e0b9a3563aa7a67ffa35cd7839a149
David <Daveysdownfall@gmail.com>
2019-04-23 18:16:27 -0400

    Added question removal

--- src/app/question-creator/question-creator.component.ts
+++ src/app/question-creator/question-creator.component.ts
@@ -26,6 +26,14 @@ export class QuestionCreatorComponent implements OnInit {
   addQuestion() {
     this.CurrentQuestionSet.questions.push(
         <Question>{text: 'Question text goes here!', answers: [{}]});
+    console.log(
+        'Question ' + this.CurrentQuestionSet.questions.length +
+        ' successfully added.');
+  }
+  removeQuestion(index: number) {
+    console.log('Question removal request recieved at index ' + index);
+    console.log(this.CurrentQuestionSet.questions.splice(index, 1));
+    console.log('Question ' + index + ' was successfully removed.');
   }
 
   ngOnInit() {}
